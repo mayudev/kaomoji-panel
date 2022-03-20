edcdeabe6e939316d5683dfe9babf55ab9c26ff4
mayudev <99561108+mayudev@users.noreply.github.com>
2022-03-20 22:39:52 +0100

    feat: add repo link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
 import "../styles/header.scss";
+import code from "../code.svg";
 
 function Header() {
   return (
     <div className="Header">
       <span className="Header__text">Kaomoji Panel</span>
+      <span style={{ flex: 1 }}></span>
+      <a
+        href="https://github.com/mayudev/kaomoji-panel"
+        target="_blank"
+        rel="noreferrer"
+        className="Header__icon"
+      >
+        <img src={code} alt="Source code" />
+      </a>
     </div>
   );
 }
