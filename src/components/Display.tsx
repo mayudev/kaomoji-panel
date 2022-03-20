04741b65e9e04232d2add94b49449304ade077e8
mayudev <99561108+mayudev@users.noreply.github.com>
2022-03-20 11:36:47 +0100

    feat: adjust Display to work with Group

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,15 +1,41 @@
-import emotes from "../lib/emotes";
-import Kaomoji from "./Kaomoji";
+import { group1, group2, group3, group4 } from "../lib/emotes";
+import Group from "./Group";
+import "../styles/display.scss";
 
 function Display() {
-  function copy(emote: string) {
+  /**
+   * Copy emoji to clipboard
+   */
+  const copy = (emote: string) => {
     navigator.clipboard.writeText(emote);
-  }
+  };
 
-  const display = emotes.map((emoji) => (
-    <Kaomoji key={emoji} onClick={(emote) => copy(emote)} content={emoji} />
-  ));
-  return <div>{display}</div>;
+  const groups = [group1, group2, group3, group4];
+
+  const display = groups.map((group) => {
+    if (group.length === 0) return false;
+
+    // First element of the array is the group name
+    const groupName = group[0];
+
+    return (
+      <Group
+        name={groupName}
+        key={groupName}
+        content={group.slice(1)}
+        onClick={(kao) => copy(kao)}
+      />
+    );
+  });
+
+  return (
+    <>
+      <div className="Display">{display}</div>
+      <footer>
+        <a href="#Group3">group3</a>
+      </footer>
+    </>
+  );
 }
 
 export default Display;
