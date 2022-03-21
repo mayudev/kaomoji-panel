ae5bf3171ccadc0a1717d5e6bb026a153c2c50bb
mayudev <99561108+mayudev@users.noreply.github.com>
2022-03-21 17:13:19 +0100

    fix(svg): use jsx tags

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -35,7 +35,7 @@ function Navigation(props: Props) {
           {/* Material Icons arrow_back_ios_new */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            enable-background="new 0 0 24 24"
+            enableBackground="new 0 0 24 24"
             height="24px"
             viewBox="0 0 24 24"
             width="24px"
@@ -54,7 +54,7 @@ function Navigation(props: Props) {
           {/* Material Icons arrow_forward_ios */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            enable-background="new 0 0 24 24"
+            enableBackground="new 0 0 24 24"
             height="24px"
             viewBox="0 0 24 24"
             width="24px"
