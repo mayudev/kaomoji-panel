6c4eef061d7d1e68b5da98894e689167b1af42a9
mayudev <99561108+mayudev@users.noreply.github.com>
2022-03-26 11:08:49 +0100

    feat: add basic styling for preferences

---
+++ src/components/preferences/BackButton.tsx
@@ -0,0 +1,9 @@
+function BackButton() {
+  return (
+    <button className="BackButton">
+      <p className="BackButton__content">back</p>
+    </button>
+  );
+}
+
+export default BackButton;
--- src/pages/Preferences.tsx
+++ src/pages/Preferences.tsx
@@ -1,5 +1,16 @@
-function Preferences() {
-  return <div>Preferences</div>;
+import BackButton from "../components/preferences/BackButton";
+import "../styles/preferences.scss";
+
+type Props = {
+  onReturn: () => void;
+};
+
+function Preferences(props: Props) {
+  return (
+    <>
+      <BackButton />
+    </>
+  );
 }
 
 export default Preferences;
