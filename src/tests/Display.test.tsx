f8302e23e05d132d22246b7ed60f5dc56a550f7a
mayudev <99561108+mayudev@users.noreply.github.com>
2022-09-13 15:44:13 +0200

    test: add test for reorder

--- src/tests/Display.test.tsx
+++ src/tests/Display.test.tsx
@@ -1,8 +1,28 @@
-import { render, screen } from "@testing-library/react";
+/* eslint-disable testing-library/no-node-access */
+import { fireEvent, render, screen } from "@testing-library/react";
 import Display from "../pages/Display";
 
 it("should display a cute kaomoji", () => {
   render(<Display />);
   const cute = screen.getAllByText("(✿ ♥‿♥)")[0];
   expect(cute).toBeInTheDocument();
+});
+
+it("should reorder items properly", () => {
+  render(<Display />);
+
+  const nav = screen.getByRole("navigation");
+  let categories = Array.from(nav.getElementsByTagName("a"));
+
+  const standardCat = categories[0];
+  const neutralCat = categories[3];
+  expect(standardCat.textContent).toEqual("Standard");
+  expect(neutralCat.textContent).toEqual("Neutral");
+
+  fireEvent.dragStart(neutralCat);
+  fireEvent.drop(standardCat);
+
+  categories = Array.from(nav.getElementsByTagName("a"));
+  expect(categories[1].textContent).toEqual("Standard");
+  expect(categories[0].textContent).toEqual("Neutral");
 });
